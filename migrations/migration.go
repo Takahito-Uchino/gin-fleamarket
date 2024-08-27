9d8cf7319ff5a353e1a449573fc69a111caefd8b
Taka <takahito.uchino.1218@gmail.com>
2024-08-27 14:58:06 +0900

    DBマイグレーション

---
+++ migrations/migration.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/Takahito-Uchino/gin-fleamarket/infra"
+	"github.com/Takahito-Uchino/gin-fleamarket/models"
+)
+
+func main() {
+	infra.Initialize()
+	db := infra.SetupDB()
+
+	if err := db.AutoMigrate(&models.Item{}); err != nil {
+		panic("Failed to migrate database")
+	}
+}
+
