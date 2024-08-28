f384ff05e6f1631cc4c4d4b6c11f8bb9b05a9587
Taka <takahito.uchino.1218@gmail.com>
2024-08-28 10:56:15 +0900

    テストに向けたリファクタリング

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Takahito-Uchino/gin-fleamarket/infra"
 	"github.com/Takahito-Uchino/gin-fleamarket/middlewares"
 	"github.com/gin-contrib/cors"
+	"gorm.io/gorm"
 
 	// "github.com/Takahito-Uchino/gin-fleamarket/models"
 	"github.com/Takahito-Uchino/gin-fleamarket/repositories"
@@ -12,17 +13,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	infra.Initialize()
-	db := infra.SetupDB()
-
-	// items := []models.Item{
-	// 	{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
-	// 	{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
-	// 	{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
-	// }
-
-	// itemRepository := repositories.NewItemMemoryRepository(items)
+func setupRouter(db *gorm.DB) *gin.Engine {
 	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
@@ -45,5 +36,21 @@ func main() {
 
 	authRouter.POST("/signup", authController.Signup)
 	authRouter.POST("/login", authController.Login)
+
+	return r
+}
+
+func main() {
+	infra.Initialize()
+	db := infra.SetupDB()
+	r := setupRouter(db)
+
+	// items := []models.Item{
+	// 	{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
+	// 	{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: true},
+	// 	{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: false},
+	// }
+
+	// itemRepository := repositories.NewItemMemoryRepository(items)
 	r.Run("localhost:8080")
 }
